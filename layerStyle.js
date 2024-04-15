8b63198c9ef95d96dc830468cd517fb7ad5ab70f
AbouMounir <harounaibncoulibaly@gmail.com>
2024-04-15 17:46:26 +0000

    enlever les icones

--- layerStyle.js
+++ layerStyle.js
@@ -1,5 +1,5 @@
 let district = {
-  color: "violet",
+  color: "black",
   weight: "2",
   opacity: 0.7,
   fillOpacity: 0,
@@ -52,4 +52,13 @@ let quartiers = {
   color: "teal",
   weight: "1",
   opacity: 0.7,
+};
+
+var geojsonMarkerOptions = {
+  radius: 3,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8,
 };
